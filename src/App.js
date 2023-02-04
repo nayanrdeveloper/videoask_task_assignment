3930067703a08255f09151e6efd4d322449dc7bf
nayanrdeveloper <nayanrdeveloper@gmail.com>
2023-02-04 13:25:36 +0530

    [FIX] remove unused code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import "./App.css";
-import VideoPlayer from "./components/VideoPlayer";
 import Campaign from "./pages/Campaign";
 import Webinar from "./pages/Webinar";
 import Welcome from "./pages/Welcome";
 
-import { store } from "./store";
-import { useSelector, useDispatch } from "react-redux";
+
+import { useSelector } from "react-redux";
 
 function App() {
   const pageData = useSelector((state) => state.page);
   return (
-    <>
+    <div>
       {pageData.welcomePage && <Welcome />}
       {pageData.webinarPage && <Webinar />}
       {pageData.campaignPage && <Campaign />}
       {pageData.thirdChoicePage && <Webinar />} 
-      {/* <Webinar /> */}
-      {/* <Campaign /> */}
-      {/* <Welcome /> */}
-    </>
+    </div>
   );
 }
 
